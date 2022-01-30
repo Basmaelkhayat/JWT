334c2a2e8c384020fb7407e12b1dc72bb12cf4cc
Basma elkhayat <b.khayat@myzeal.app>
2022-01-30 18:59:55 +0200

    update template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
-import { LoginComponent } from './login';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+// if yoy want to use the real backend, just delete these three files ⬆️
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-  ],
-  declarations: [AppComponent, HomeComponent, LoginComponent],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    imports: [BrowserModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule],
+    declarations: [AppComponent, HomeComponent, LoginComponent],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
-    // provider used to create fake backend
-    fakeBackendProvider,
-  ],
-  bootstrap: [AppComponent],
+        // provider used to create fake backend
+        fakeBackendProvider,
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { User } from '../_models';
-import { UserService, AuthenticationService } from '../_services';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
-  loading = false;
-  users: User[];
+    loading = false;
+    users: User[];
 
-  constructor(private userService: UserService) {}
+    constructor(private userService: UserService) {}
 
-  ngOnInit() {
-    this.loading = true;
-    this.userService
-      .getAll()
-      .pipe(first())
-      .subscribe((users) => {
-        this.loading = false;
-        this.users = users;
-      });
-  }
+    ngOnInit() {
+        this.loading = true;
+        this.userService
+            .getAll()
+            .pipe(first())
+            .subscribe((users) => {
+                this.loading = false;
+                this.users = users;
+            });
+    }
 }
